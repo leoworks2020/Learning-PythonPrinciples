595d1b168f5b79a4fc01db58b83192bcbaeebe21
leoworks2020 <leoworks2020@gmail.com>
2023-07-13 22:20:16 -0300

    Resolved Challenge 003

---
+++ 003.PythonPrinciplesChallenge.OnlineStatus.py
@@ -0,0 +1,43 @@
+"""
+Python Principles Challenge
+-----------------------------------------------------------------------------------------------
+Challenge:
+Online status
+The aim of this challenge is, given a dictionary of people's online status,
+to count the number of people who are online.
+
+For example, consider the following dictionary:
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+
+In this case, the number of people online is 2.
+
+Write a function named online_count that takes one parameter.
+The parameter is a dictionary that maps from strings of names to the string "online" or "offline",
+as seen above.
+
+Your function should return the number of people who are online.
+-----------------------------------------------------------------------------------------------
+Challenge Start Date:   2023-07-13
+Challenge End Date:     2023-07-13
+"""
+def online_count(students_dict):
+
+    value_counter = 0
+    for key in students_dict:
+        if students_dict[key] == "online":
+            value_counter += 1
+    return value_counter
+
+
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+
+total_online = online_count(statuses)
+print(total_online)
